ed4e0c498feb20a15fabe893f1c3c624d1494630
Bușilă George <goldjoker43@gmail.com>
2024-07-25 22:34:00 +0300

    Spring initializer

---
+++ src/main/java/api/bsstrader/BsstraderApplication.java
@@ -0,0 +1,13 @@
+package api.bsstrader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BsstraderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BsstraderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/api/bsstrader/BsstraderApplicationTests.java
@@ -0,0 +1,13 @@
+package api.bsstrader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BsstraderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
