0871306e9e32dcbd253bea77beec935ab4045b20
Bușilă George <goldjoker43@gmail.com>
2024-07-27 15:54:38 +0300

    Added @Repository annotation

--- src/main/java/api/bsstrader/repo/StickerRepo.java
+++ src/main/java/api/bsstrader/repo/StickerRepo.java
@@ -2,6 +2,8 @@ package api.bsstrader.repo;
 
 import api.bsstrader.entity.Sticker;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface StickerRepo extends JpaRepository<Sticker, Long> {
 }
