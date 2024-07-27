58c068c636261d1527072f4b6e29efe1a40a46c9
Bușilă George <goldjoker43@gmail.com>
2024-07-27 22:04:28 +0300

    Added list of owners to stickers

--- src/main/java/api/bsstrader/entity/Sticker.java
+++ src/main/java/api/bsstrader/entity/Sticker.java
@@ -5,6 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -19,4 +21,6 @@ public class Sticker {
 
     @Enumerated(EnumType.STRING)
     private StickerType type;
+
+    private List<Player> owners;
 }
